cafd8a94639c393395bd10aff2336fc7ba9ccece
jinan159 <wlsdhks0423@naver.com>
2021-11-09 08:56:17 +0900

    [Chapter03] Modify Posts_updated test case

--- src/test/java/com/jinan159/study/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/jinan159/study/springboot/web/PostsApiControllerTest.java
@@ -17,12 +17,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.xml.ws.Response;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -68,7 +65,6 @@ public class PostsApiControllerTest {
     @Test
     public void Posts_updated() throws Exception {
         //given
-
         Posts savedPosts = postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
@@ -77,28 +73,26 @@ public class PostsApiControllerTest {
 
         Long updateId = savedPosts.getId();
         String expectedTitle = "title2";
-        String expextexContent = "content2";
+        String expectedContent = "content2";
 
+        //when
         PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
                 .title(expectedTitle)
-                .content(expextexContent)
+                .content(expectedContent)
                 .build();
 
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
-
         HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
 
+        // then
         assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-
         assertThat(responseEntity.getBody()).isGreaterThan(0L);
 
+        // then
         List<Posts> all = postsRepository.findAll();
-
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-
-        assertThat(all.get(0).getContent()).isEqualTo(expextexContent);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
 
 }
