c0cb366a96e012d635a06aae11cfb939d8ba0bd7
jinan159 <wlsdhks0423@naver.com>
2021-11-08 23:14:55 +0900

    Add Chapter03 Add PostsServiceTest

---
+++ src/test/java/com/jinan159/study/springboot/service/posts/PostsServiceTest.java
@@ -0,0 +1,44 @@
+package com.jinan159.study.springboot.service.posts;
+
+import com.jinan159.study.springboot.web.dto.PostsResponseDto;
+import com.jinan159.study.springboot.web.dto.PostsSaveRequestDto;
+import com.jinan159.study.springboot.web.dto.PostsUpdateRequestDto;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Test
+    public void Insert_Update_Select_Test() {
+        Long savedPostsId = postsService.save(PostsSaveRequestDto.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        PostsResponseDto resposeDto = postsService.findById(savedPostsId);
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        Long updatedId = postsService.update(resposeDto.getId(), PostsUpdateRequestDto.builder()
+                .id(resposeDto.getId())
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build());
+
+        PostsResponseDto responseDto = postsService.findById(updatedId);
+
+        assertThat(responseDto.getTitle()).isEqualTo(expectedTitle);
+        assertThat(responseDto.getContent()).isEqualTo(expectedContent);
+    }
+}
