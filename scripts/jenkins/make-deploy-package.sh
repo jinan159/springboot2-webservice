d83c22878610aad896b76f6b7ab5c49fabcb3964
jinan159 <wlsdhks0423@naver.com>
2021-11-16 10:39:05 +0900

    Add packaging and deploy script

--- scripts/jenkins/make-deploy-package.sh
+++ scripts/jenkins/make-deploy-package.sh
@@ -3,19 +3,22 @@
 PROJECT_NAME=springboot2-webservice
 JENKINS_HOME=/var/jenkins_home
 
-BEFORE_DEPLOY=$JENKINS_HOME/before-deploy
 WORKSPACE_ROOT=$JENKINS_HOME/workspace/$PROJECT_NAME
+BEFORE_DEPLOY=$WORKSPACE_ROOT/before-deploy
 
-# make directory before-deploy/script/service
-if [ ! -d "$BEFORE_DEPLOY/jar" ]
-then
+echo ">>>> START MAKE DEPLOY PACKAGE <<<<"
+
+if [ ! -d "$BEFORE_DEPLOY/jar" ]; then
+  echo ">> Make $BEFORE_DEPLOY/jar directory"
   mkdir -p $BEFORE_DEPLOY/jar
 fi
+rm -rf $BEFORE_DEPLOY/jar/*
 
-if [ ! -d "$BEFORE_DEPLOY/scripts/service" ]
-then
+if [ ! -d "$BEFORE_DEPLOY/scripts/service" ]; then
+  echo ">> Make $BEFORE_DEPLOY/scripts/service directory"
   mkdir -p $BEFORE_DEPLOY/scripts/service
 fi
+rm -rf $BEFORE_DEPLOY/scripts/service
 
 echo ">> Copy jar file."
 cp $WORKSPACE_ROOT/build/libs/*.jar $BEFORE_DEPLOY/jar
@@ -27,5 +30,7 @@ echo ">> Move to before-deploy directory."
 cd $BEFORE_DEPLOY
 echo $(pwd)
 
-echo ">> Archive $PROJECT_NAME.tar.gz"
+echo ">> Zip scripts and jar to $PROJECT_NAME.tar.gz"
 tar -zcvf $PROJECT_NAME.tar.gz ./scripts ./jar
+
+echo ">>>> END MAKE DEPLOY PACKAGE <<<<"
---
+++ scripts/service/deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+APP_HOME=/home/webservice/app
+
+PROJECT_NAME=springboot2-webservice
+JAR_DIR=$APP_HOME/jar
+CONFIG_DIR=$APP_HOME/config
+
+echo ">>>> START DEPLOY SERVER <<<<"
+
+echo ">> Check [$PROJECT_NAME] running state.";
+
+echo ">> Check [$PROJECT_NAME] running state.";
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
+
+if [ -z "$CURRENT_PID" ]; then
+  echo ">> [$PROJECT_NAME] is not running."
+else
+  echo ">> Kill application for deploy(PID:$CURRENT_PID)"
+  echo ">> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo ">> Start deploy [$PROJECT_NAME]"
+
+JAR_NAME=$(ls -tf JAR_DIR/*.jar | tail -n 1)
+echo ">> Deploy target : [$JAR_NAME]"
+
+echo ">> Add execution permission to [$JAR_NAME]"
+chmod +x $JAR_NAME
+
+echo ">> Run [$JAR_NAME]"
+nohup java -jar \
+-Dspring.config.location=classpath:/application-real.properties,$CONFIG_DIR/application-real-db.properties,$CONFIG_DIR/application-oauth.properties, \
+-Dspring.profiles.active=real \
+$JAR_NAME > nohup.out &
+
+echo ">>>> END DEPLOY SERVER <<<<"
+
