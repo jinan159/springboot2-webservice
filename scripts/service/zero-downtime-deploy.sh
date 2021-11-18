6a948b29c4608115fe68d0649210306eb9ef0d0e
jinan159 <wlsdhks0423@naver.com>
2021-11-18 16:30:14 +0900

    Add script authorize command

--- scripts/service/zero-downtime-deploy.sh
+++ scripts/service/zero-downtime-deploy.sh
@@ -3,13 +3,23 @@
 
 echo ">>>> START ZERO DOWNTIME DELPOY SCRIPT <<<<"
 
+echo "> Authorize execute permission to scripts"
+chmod +x ./task/health.sh
+chmod +x ./task/profile.sh
+chmod +x ./task/start.sh
+chmod +x ./task/stop.sh
+chmod +x ./task/switch.sh
+
 # stop unused service
+echo "> timeout 60s ./task/stop.sh"
 timeout 60s ./task/stop.sh
 
 # start new version of server
+echo "> timeout 60s ./task/start.sh"
 timeout 60s ./task/start.sh
 
 # health check new version of service
+echo "> timeout 60s ./task/health.sh"
 timeout 60s ./task/health.sh
 
 echo ">>>> END ZERO DOWNTIME DELPOY SCRIPT <<<<"
